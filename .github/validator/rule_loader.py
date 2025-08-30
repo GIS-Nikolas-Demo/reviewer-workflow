72e1c7b6f94c0a238c0de56c91e41f979f0b698f
jyancesegama <nmendozaricaldi@gmail.com>
2025-08-30 01:26:32 -0500

    q

--- .github/validator/rule_loader.py
+++ .github/validator/rule_loader.py
@@ -3,19 +3,17 @@ import yaml
 
 
 def get_rules_for_dependencies(dependencies,rules_path):
-    print("CWD:", os.getcwd())
-    print("Files in CWD:", os.listdir("."))
     applied_rules = {}
-
     for file_name in os.listdir(rules_path):
         if file_name.endswith("-rules.yml"):
             file_path = os.path.join(rules_path, file_name)
-
+            # nombre lógico de la regla quitando el sufijo
+            rule_key = file_name.replace("-rules.yml", "")
             with open(file_path, "r") as f:
                 data = yaml.safe_load(f)
-
-                file_deps = data.get("dependencies", [])
-                file_rules = data.get("rules", {})
+                rules = data.get(rule_key, {})
+                file_deps = rules.get("dependencies", [])
+                file_rules = rules.get("rules", {})
 
                 # Verifica si alguna dependencia coincide
                 if any(dep in file_deps for dep in dependencies):
